da78ec0eac8d54b15780fc6c5ca762972be4a5fa
jislacity <122810100+jislacity@users.noreply.github.com>
2023-03-30 10:08:04 -0500

    Wush

--- core/util/debug/trace_helper.py
+++ core/util/debug/trace_helper.py
@@ -1,4 +1,9 @@
 import traceback
+import datetime
+import re
+import pytz
+
+from core.util.path.path_helper import PathHelper
 
 
 class TraceHelper:
@@ -6,6 +11,9 @@ class TraceHelper:
         self.message = ""
         self.trace = ""
         self.image = ""
+        self.log_lines = []
+        tz = pytz.timezone('America/Bogota')
+        self.now = datetime.datetime.now(tz).strftime('%Y%m%d%H%M%S')
 
     @staticmethod
     def get_trace_str(e):
@@ -13,6 +21,26 @@ class TraceHelper:
         return traceback_str
 
     @staticmethod
-    def trace_str(self, e):
-        traceback_str = ''.join(traceback.format_tb(e.__traceback__))
-        self.trace = traceback_str
+    def xml_escape(chars, data_dict):
+        return chars.encode('ascii', 'xmlcharrefreplace').decode()
+
+    def log(self, text="", message=""):
+        # Get current date and time
+        path_helper = PathHelper()
+        path = f'{path_helper.get_project_root_path()}/storage/logs'
+        folder_path = path_helper.create_directory(path)
+        with open(f'{folder_path}/log_{self.now}.html',
+                  'a', encoding='utf-8') as file:
+            self.log_lines.append(text)
+            self.log_lines.append(message)
+            file.write(f'{text}\n')
+            file.write(f'{message}\n')
+
+    def extract_url_from_styles(self, text=""):
+        if text != "":
+            url_re = re.search(r'url\((.*?)\)', text)
+            if url_re is not None:
+                url = url_re.group(1)
+                return url
+        else:
+            return "ERROR"
--- core/util/path/path_helper.py
+++ core/util/path/path_helper.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import os
 
-
 class PathHelper:
     def __init__(self):
         pass
@@ -14,3 +13,15 @@ class PathHelper:
     def create_directory(path):
         if not os.path.exists(path):
             os.makedirs(path)
+        return path
+
+    @staticmethod
+    def create_file(path, name, content, extension):
+        # create a new file with the given extension in the current directory
+        if f".{extension}" in name:
+            new_file_path = os.path.join(path, f"{name}")
+        else:
+            new_file_path = os.path.join(path, f"{name}.{extension}")
+        with open(new_file_path, "w", encoding='utf-8') as f:
+            f.write(content)
+        return new_file_path
